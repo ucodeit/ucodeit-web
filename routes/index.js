4678fa2931d63c1841d15fd168c7a8f7c1937845
zeris <brandon-gg1@outlook.com>
2022-07-05 23:04:07 -0500

    feat: add middleware and migration of html to ejs logic

---
+++ routes/index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+    title: 'UCode-It', 
+    eventName: 'index', 
+    jsFiles: ['index'], 
+    styleSheetFiles: ['main', 'index'] 
+  });
+});
+
+router.get('/index', function(req, res, next) {
+  res.render('index', { 
+    title: 'UCode-It', 
+    eventName: 'index', 
+    jsFiles: ['index'], 
+    styleSheetFiles: ['main', 'index'] 
+  });
+});
+
+router.get('/about_us', function(req, res, next) {
+  res.render('about_us', { 
+    title: 'Acerca de nosotros', 
+    eventName: 'aboutus',
+    jsFiles: ['aboutus'],
+    styleSheetFiles: ['aboutus', 'main']
+  });
+});
+
+router.get('/eventos', function(req, res, next) {
+  res.render('eventos', { 
+    title: 'Eventos', 
+    eventName: 'eventos',
+    jsFiles: ['ajuste'],
+    styleSheetFiles: ['eventos', 'main', 'index']
+  });
+});
+
+router.get('/formulario', function(req, res, next) {
+  res.render('formulario', { 
+    title: 'Unete', 
+    eventName: 'formulario',
+    jsFiles: ['formulario'],
+    styleSheetFiles: ['formulario', 'main', 'index']
+  });
+});
+
+module.exports = router;
