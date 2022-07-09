72481f069e05417a11d2ab0300d149948b8c58ee
lSheel <shee.cert@gmail.com>
2022-07-09 03:53:43 -0500

    Envio de correo y corrección de errores

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
-
+var form = require('./routes/form');
 var app = express();
 
 // view engine setup
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-
+app.use('/formulario', form);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
