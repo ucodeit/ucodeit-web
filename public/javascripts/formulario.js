ee0369f2f933d1ed57c203e03d0aeb29e10b453e
lSheel <shee.cert@gmail.com>
2022-07-11 22:16:34 -0500

    Validación de formulario parte JS

--- public/javascripts/formulario.js
+++ public/javascripts/formulario.js
@@ -1,109 +1,64 @@
-/* variables de insercion de datos 
-aqui primero voy a aobtener en un array vacio los datos del DOM ya que este array funcionara
-como variable volatil */
-var arrayPostulante = [];
+const validarDatos = () => {
+  emailRegex =
+    /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i;
+  const alert = document.querySelector("#alert");
+  let postulante = {
+    nombre: document.querySelector("#nombre").value.trim(),
+    telefono: document.querySelector("#telefono").value.trim(),
+    email: document.querySelector("#email").value.trim(),
+    carrera: document.querySelector("#carrera").value.trim(),
+    semestre: document.querySelector("#semestre").value.trim(),
+    skills: document.querySelector("#skills").value.trim(),
+    aboutYou: document.querySelector("#aboutYou").value.trim(),
+  };
 
-/* posteriormente se traslada aun json para que se organice como string y con ese se trabaje a manera de objeto */
-var postulante = {
-  nombre: " ",
-  telefono: " ",
-  email: " ",
-  carrera: " ",
-  semestre: " ",
-  skills: " ",
-  aboutYou: " ",
-};
-
-/* controladores de obtencion de datos mediante manejo de DOM lo que se esta haciendo en la primera version es 
-obtener los datos dentro de un array, y esos datos pasarselos a un json, posteriormente se borra el array para que solo se
-conserve el json, de esta manera, si es necesario se puede dividir el programa en varios scripts para que no se vea la info*/
-/* para que este archivo tenga un mejor orden, debe de estar dividido en funciones */
-// var submitForm = document.querySelector("#formPostulantes");
-
-
-let form = document.getElementById("formPostulantes");
-if(form != null){
-    form.addEventListener("submit", obtencionDatos);
-    console.log("Ejemplo");
-}
-
-/* aqui separarremos  */
-function obtencionDatos() {
-  /*En el futuro mejorar la validación para que esta pueda aceptar almenos el + por aquello de las LADAS*/
-  if (
-    isNaN(document.querySelector("#nombre").value) &&
-    !isNaN(document.querySelector("#telefono").value)
-  ) {
-    arrayPostulante.push(document.querySelector("#nombre").value);
-    arrayPostulante.push(document.querySelector("#telefono").value);
-    arrayPostulante.push(document.querySelector("#mail").value);
-    arrayPostulante.push(document.querySelector("#carrera").value);
-    arrayPostulante.push(document.querySelector("#semestre").value);
-    arrayPostulante.push(document.querySelector("#skills").value);
-    arrayPostulante.push(document.querySelector("#aboutYou").value);
-
-    if (
-      arrayPostulante[0].trim() != "" &&
-      arrayPostulante[1].trim() != "" &&
-      arrayPostulante[2].trim() != "" &&
-      arrayPostulante[5].trim() != "" &&
-      arrayPostulante[6].trim() != ""
-    ) {
-      /* console.log(arrayPostulante); esta linea es para comprobacion obtenciocn de datos*/
-      guardadoDatos();
-      /* console.log(postulante); esta linea es para comprobacion de traspaso de datos*/
-      arrayPostulante = [];
-    } else {
-      alert("Favor de Introducir los datos necesarios");
-      arrayPostulante = [];
+  for (const obj in postulante) {
+    if (postulante[obj] === "") {
+      alert.innerHTML =
+        '<div class="alert alert-primary d-flex align-items-center" role="alert">' +
+        '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">' +
+        '<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>' +
+        "</svg>" +
+        "<div>" +
+        "Faltan campos por llenar" +
+        "</div>" +
+        "</div>";
+      window.scroll(0, 0);
+      return false;
     }
-  } else {
-    alert("La información introducida en el formulario es invalida");
+    if (
+      (obj === "nombre" &&
+        (/\d/.test(postulante[obj]) || postulante[obj].length < 3)) ||
+      (obj === "telefono" &&
+        (isNaN(postulante[obj]) || postulante[obj].length < 10))
+    ) {
+      alert.innerHTML =
+        '<div class="alert alert-primary d-flex align-items-center" role="alert">' +
+        '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">' +
+        '<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>' +
+        "</svg>" +
+        "<div>" +
+        "Verifique sus datos, uno o más no son válidos" +
+        "</div>" +
+        "</div>";
+      window.scroll(0, 0);
+      return false;
+    }
   }
-}
 
-function guardadoDatos() {
-  postulante.nombre = arrayPostulante[0].trim();
-  postulante.telefono = arrayPostulante[1].trim();
-  postulante.email = arrayPostulante[2].trim();
-  postulante.carrera = arrayPostulante[3].trim();
-  postulante.semestre = arrayPostulante[4].trim();
-  postulante.skills = arrayPostulante[5].trim();
-  postulante.aboutYou = arrayPostulante[6].trim();
-}
+  if (!emailRegex.test(postulante.email)) {
+    alert.innerHTML =
+      '<div class="alert alert-primary d-flex align-items-center" role="alert">' +
+      '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">' +
+      '<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>' +
+      "</svg>" +
+      "<div>" +
+      "Introduzca un email válido" +
+      "</div>" +
+      "</div>";
+    window.scroll(0, 0);
+    return false;
+  }
 
-//[> Este es una llamada a la api EmailJS, se espera que esta api pueda recibir los datos del formulario, sin embargo
-//hay que comprobar si los recibe desde el form o desde el JSON */
-//function enviarEmail(event) {
-  //[> la funcion init permite establecer la conexion con las promesas de la appi, si no se usara esta funcion
-    //solo hay que poner el User id del init como 4to parametro de la funcion send o send form */
-    //console.log("Se esta ejecutando");
-  //event.preventDefault();
-  ////emailjs.init("Va0gnkrE16yx6ZTiH");
-  //emailsjs.init("cxFoL33icVu_kmj0c")
-  //obtencionDatos();
-
-  //btnEnviar.value = "Enviando...";
-  //[> estas variables son las que se necesitan para obtener el servicio de la cuenta y poder enviarlos a la funcion.
-        //Sin embargo el servicio puede ser cambiado en el futuro si se establece un correo para ucodeit solo para poder usar
-        //emailjs ya que ahorita utilizar mi correo personal como servicio intermedio*/
-  //const serviceID = "service_v4il83b"
-  //const templateID = "template_i0qalvf" 
-  ////const serviceID = "service_8os59jx";
-  ////const templateID = "template_nls1hzv";
-  //[> a diferencia de sendForm este metodo permite el envio de mensaje con parametros provenientes de otro lugar
-        //que no es el form directamente, de esta manera se supone el script funciona. */
-  //emailjs.send(serviceID, templateID, postulante).then(
-    //() => {
-      //btnEnviar.value = "Enviar mensaje";
-      //alert(
-        //"Formulario enviado, Bienvenido tu mensaje sera respondido en un plazo de 72 horas"
-      //);
-      //[> console.log(emailjs); Este console. log es para encontrar si es que enserio recibe dichos parametros<]
-    //},
-    //(err) => {
-      //btnEnviar.value = "Enviar mensaje";
-      //alert(JSON.stringify(err));
-    //}
-  //);
-//}
+  return true;
+};
