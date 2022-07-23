3c7851fe38fcced55805ae20288b89504f5d5023
lSheel <shee.cert@gmail.com>
2022-07-23 12:22:56 -0500

    Redireccionamiento de formulario

--- routes/form.js
+++ routes/form.js
@@ -2,19 +2,24 @@ const express = require("express");
 const transporter = require("./../src/config/mail");
 const Joi = require("joi");
 const router = express.Router();
-const app = express();
 //Se crea un esquema Joi en donde definimos los campos que vamos a validar
 const schema = Joi.object({
   //Nombre será un string con un minimo de 3 caracteres y la regex lo que va aceptar es palabras de la Aa-Zz, mas especiales y espacios
-  nombre: Joi.string().min(3).pattern(/^[a-zA-Z\u00C0-\u017F\s]+$/).required(),
+  nombre: Joi.string()
+    .min(3)
+    .pattern(/^[a-zA-Z\u00C0-\u017F\s]+$/)
+    .required(),
   //telefono lo manejaremos como string para que puedar usar el metodo lenght para el tamaño,y el regex significa que solo acpeta numeros
   telefono: Joi.string().length(10).pattern(/^\d+$/).required(),
   //Email igual sera un string donde el dominio minimo tiene que tener dos caracteres y el regex solo acepta alfanumericos, cualquier caracter
   //especial mandara error
-  email: Joi.string().email({
-    minDomainSegments: 2,
-  }).pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/).required(),
-  //Los demás no tiene algo agregado solo son requeridos los campos de skills y about you tiene que tener min 10 caracteres  
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+    })
+    .pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+    .required(),
+  //Los demás no tiene algo agregado solo son requeridos los campos de skills y about you tiene que tener min 10 caracteres
   carrera: Joi.string().required(),
   semestre: Joi.number().required(),
   skills: Joi.string().min(10).required(),
@@ -27,7 +32,22 @@ router.get("/", function (req, res, next) {
     eventName: "formulario",
     jsFiles: ["formulario"],
     styleSheetFiles: ["formulario", "main", "index"],
+    alert: undefined,
   });
+});
+
+router.get("/response", function (req, res, next) {
+  cookie = req.flash("res");
+  if ( !cookie.includes("true")) {
+    res.redirect("/formulario");
+  } else {
+    res.render("response", {
+      title: "Sus datos han sido enviados",
+      eventName: "aboutus",
+      jsFiles: ["aboutus"],
+      styleSheetFiles: ["aboutus", "main"],
+    });
+  }
 });
 
 router.post("/", (req, res, next) => {
@@ -52,18 +72,23 @@ router.post("/", (req, res, next) => {
       //Si se cumple
       .then((response) => {
         console.log(response);
-        //res.send('<script>alert("Tus datos han sido enviados"); window.location.href = "/formulario"; </script>');
-        res.json({
-          res : response,
-        });
+        req.flash("res", "true");
+        res.redirect("/formulario/response");
       })
       //Si manda error
       .catch((err) => {
-        res.status(404).json({error : err});
+        console.log(err);
+        res.status(404).render("formulario", {
+          title: "Unete",
+          eventName: "formulario",
+          jsFiles: ["formulario"],
+          styleSheetFiles: ["formulario", "main", "index"],
+          alert: true,
+        });
       });
     //Si manda error la validación de datos
   } else {
-    res.status(404).json({ error: error });
+    res.status(400).json({ error: error });
   }
 });
 
